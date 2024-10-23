d3e2716ed2ee2986240d1394a34f3d62b8152411
MarselDokubo <connectmarsel@gmail.com>
2024-10-23 18:27:18 +0100

    linking db to ui

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 - [x] Make it deploy (Vercel)
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Set up the database (w/ Vercel postgress)
-- [ ] Attach the database to ui
+- [x] Set up the database (w/ Vercel postgress)
+- [x] Attach the database to ui
 - [ ] Add Authentication (w/ Clerk)
 - [ ] Add image upload
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,14 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/V2uZaaFRHKJT15DuRGKDCdx4O9iTLG3PIykaHz5Q6YNtw7KX",
-  "https://utfs.io/f/V2uZaaFRHKJTVlXiGkdFRHKJTyp9hAgw2D0t16oO5BQmlSnf",
-  "https://utfs.io/f/V2uZaaFRHKJT4RMhSaAD8aZY90xITgzQn75HGAKwtoBPNWEi",
-  "https://utfs.io/f/V2uZaaFRHKJT15DuRGKDCdx4O9iTLG3PIykaHz5Q6YNtw7KX",
-  "https://utfs.io/f/V2uZaaFRHKJTVlXiGkdFRHKJTyp9hAgw2D0t16oO5BQmlSnf",
-  "https://utfs.io/f/V2uZaaFRHKJT4RMhSaAD8aZY90xITgzQn75HGAKwtoBPNWEi",
-  "https://utfs.io/f/V2uZaaFRHKJT15DuRGKDCdx4O9iTLG3PIykaHz5Q6YNtw7KX",
-  "https://utfs.io/f/V2uZaaFRHKJTVlXiGkdFRHKJTyp9hAgw2D0t16oO5BQmlSnf",
-  "https://utfs.io/f/V2uZaaFRHKJT4RMhSaAD8aZY90xITgzQn75HGAKwtoBPNWEi",
-  "https://utfs.io/f/V2uZaaFRHKJT15DuRGKDCdx4O9iTLG3PIykaHz5Q6YNtw7KX",
-  "https://utfs.io/f/V2uZaaFRHKJTVlXiGkdFRHKJTyp9hAgw2D0t16oO5BQmlSnf",
-  "https://utfs.io/f/V2uZaaFRHKJT4RMhSaAD8aZY90xITgzQn75HGAKwtoBPNWEi",
-  "https://utfs.io/f/V2uZaaFRHKJT15DuRGKDCdx4O9iTLG3PIykaHz5Q6YNtw7KX",
-  "https://utfs.io/f/V2uZaaFRHKJTVlXiGkdFRHKJTyp9hAgw2D0t16oO5BQmlSnf",
-  "https://utfs.io/f/V2uZaaFRHKJT4RMhSaAD8aZY90xITgzQn75HGAKwtoBPNWEi",
-];
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {mockImages.map((image) => (
-        <div key={image.id} className="flex h-48 w-48 flex-col">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={index} className="flex h-48 w-48 flex-col">
           <Link href={`/img/${image.id}`}>
             <img
               src={image.url}
