ad4ed4bdf81baebe0b8035ad51203f533bc7dca0
MarselDokubo <connectmarsel@gmail.com>
2024-10-24 15:59:40 +0100

    auth setup

--- src/app/_components/top-nav.tsx
+++ src/app/_components/top-nav.tsx
@@ -1,7 +1,18 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <h2>Gallery</h2>
+      <div>Gallery</div>
+
+      <div className="flex flex-row items-center gap-4">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </nav>
   );
 }
